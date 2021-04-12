1524dccf1b86a6b716085084caad5233a7a8e98a
unsky559 <6kostya@gmail.com>
2021-04-12 23:43:57 +0300

    feat: 08 solution

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -7,7 +7,7 @@
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and l = 3,
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined with this interface
@@ -16,9 +16,43 @@
  *   this.next = null;
  * }
  */
+const ListNode = require('../extensions/list-node');
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
 
 function removeKFromList(/* l, k */) {
   throw new Error('Not implemented');
+  let current = l;
+  while(current !== null){
+    console.log(current.value)
+
+    current = current.next;
+  }
+  return l;
 }
+
+function removeKFromList(l, k) {
+  let result = l;
+  while(result !== null){
+    if (result.value === k){
+      result.value = result.next.value;
+      result.next = result.next.next;
+    }
+    result = result.next;
+  }
+  return l;
+}
+
+console.log(removeKFromList(convertArrayToList([3, 1, 2, 3, 4, 5]), 3));
 
 module.exports = removeKFromList;
