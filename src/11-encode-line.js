a17b5a8b0eb6a8a4b59b099322ddc1945c53326a
unsky559 <6kostya@gmail.com>
2021-04-12 19:25:03 +0300

    feat: 11 solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = str.split('');
+  let res = '';
+  let lastLetter = result[0];
+  let counter = 0;
+  for (let i = 0; i <= result.length; i++) {
+    if (lastLetter === result[i]) {
+      counter += 1;
+    } else {
+      if (counter > 1) {
+        res += `${counter}${lastLetter}`;
+      } else {
+        res += `${lastLetter}`;
+      }
+      lastLetter = result[i];
+      counter = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
